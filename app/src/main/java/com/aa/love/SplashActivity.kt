dfd4e6a252a27a47e7d76ff9d5d1946a4b14e281
Administrator <guadan0711@126.com>
2019-02-13 11:29:05 +0800

    Initial commit

---
+++ app/src/main/java/com/aa/love/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.aa.love
+
+import android.animation.Animator
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        with(splash) {
+            setAnimation("love.json")
+            playAnimation()
+            addAnimatorListener(object : Animator.AnimatorListener {
+                override fun onAnimationRepeat(animation: Animator?) {
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    finish()
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                }
+
+                override fun onAnimationStart(animation: Animator?) {
+                }
+            })
+        }
+    }
+
+}
