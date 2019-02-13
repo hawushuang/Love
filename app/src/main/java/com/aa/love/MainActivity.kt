0137e1a308e833f3bad3bfd5a7253656cb92d552
Administrator <guadan0711@126.com>
2019-02-13 17:20:35 +0800

    添加闪屏页

--- app/src/main/java/com/aa/love/MainActivity.kt
+++ app/src/main/java/com/aa/love/MainActivity.kt
@@ -2,11 +2,13 @@ package com.aa.love
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Explode
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.enterTransition = Explode().setDuration(2000)
     }
 }
--- app/src/main/java/com/aa/love/SplashActivity.kt
+++ app/src/main/java/com/aa/love/SplashActivity.kt
@@ -1,9 +1,11 @@
 package com.aa.love
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.transition.Fade
 import android.view.View
 import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_splash.*
@@ -13,19 +15,23 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        window.exitTransition = Fade().setDuration(2000)
         val typeface = Typeface.createFromAsset(assets, "myfont.otf")
-        tv.typeface = typeface
+        tv1.typeface = typeface
         tree_view.setReadyListener {
-            showTextView()
+            showTextView(tv1)
         }
     }
 
-    private fun showTextView() {
+    private fun showTextView(tv: View) {
         tv.visibility = View.VISIBLE
         val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
         tv.startAnimation(animation)
         tv.postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(
+                Intent(this, MainActivity::class.java),
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle()
+            )
         }, 1500L)
     }
 
